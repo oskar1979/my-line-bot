bf66648429fcd857709b2070297aed928a6bd068
oskar1979 <chiu19790202@gmail.com>
2025-07-19 13:57:30 +0800

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    console.log("收到 LINE 的訊息：", req.body);
+    console.log('Received LINE webhook:', req.body);
     res.status(200).send('OK');
   } else {
     res.status(405).end(); // Method Not Allowed
