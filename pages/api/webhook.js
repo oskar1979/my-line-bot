698d1204262b4ee5973f88b681db09b555e25f68
oskar1979 <chiu19790202@gmail.com>
2025-07-19 13:53:50 +0800

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,30 +1,8 @@
-import { buffer } from 'micro';
-import crypto from 'crypto';
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).send('Method Not Allowed');
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log("收到 LINE 的訊息：", req.body);
+    res.status(200).send('OK');
+  } else {
+    res.status(405).end(); // Method Not Allowed
   }
-
-  const buf = await buffer(req);
-  const body = buf.toString();
-  const signature = req.headers['x-line-signature'];
-
-  const hash = crypto
-    .createHmac('SHA256', process.env.LINE_CHANNEL_SECRET)
-    .update(body)
-    .digest('base64');
-
-  if (hash !== signature) {
-    return res.status(401).send('Unauthorized');
-  }
-
-  // 成功的 response
-  res.status(200).send('OK');
 }
